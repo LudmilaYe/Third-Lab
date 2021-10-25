f44346bcd23e227e0d49ed547e635f7d67f98080
LudmilaYe <yegupova.mila@bk.ru>
2021-10-25 22:41:43 +0800

    Добавлены автотесты

---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cff1b1ae-deb2-4100-9b8c-28f5d8dfe0d9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Third_Lab;
+
+namespace Tests
+{
+    [TestClass]
+    public class LogicTests
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            var firstVector = new Vector(10d, 10d, 10d);
+            var secondVector = new Vector(15d, 2d, 3d);
+
+            var resultVector = firstVector + secondVector;
+            Assert.AreEqual(25, resultVector.X);
+            Assert.AreEqual(12, resultVector.Y);
+            Assert.AreEqual(13, resultVector.Z);
+        }
+
+        [TestMethod]
+        public void TestDifference()
+        {
+            var firstVector = new Vector(10d, 10d, 10d);
+            var secondVector = new Vector(15d, 2d, 3d);
+
+            var resultVector = firstVector - secondVector;
+            Assert.AreEqual(-5, resultVector.X);
+            Assert.AreEqual(8, resultVector.Y);
+            Assert.AreEqual(7, resultVector.Z);
+        }
+
+        [TestMethod]
+        public void TestVectorProduct()
+        {
+            var firstVector = new Vector(3d, 8d, 12d);
+            var secondVector = new Vector(5d, 2d, 3d);
+
+            var resultVector = firstVector ^ secondVector;
+            Assert.AreEqual(0, resultVector.X);
+            Assert.AreEqual(24, resultVector.Y);
+            Assert.AreEqual(-34, resultVector.Z);
+        }
+
+        [TestMethod]
+        public void TestScalarProduct()
+        {
+            var firstVector = new Vector(3d, 8d, 12d);
+            var secondVector = new Vector(5d, 2d, 3d);
+
+            Assert.AreEqual(67, firstVector * secondVector);
+        }
+
+        [TestMethod]
+        public void TestLength()
+        {
+            var vector = new Vector(1d, 3d, 5d);
+
+            Assert.AreEqual(5, vector.Length);
+        }
+    }
+}
