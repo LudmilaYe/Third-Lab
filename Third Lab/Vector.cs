93f733cbefebd4be9179062357ad0a904a8014f7
LudmilaYe <yegupova.mila@bk.ru>
2021-10-25 21:12:31 +0800

    Вектор изменён на трёхмерный

--- Third Lab/Vector.cs
+++ Third Lab/Vector.cs
@@ -6,33 +6,35 @@ namespace Third_Lab
     {
         private double _x;
         private double _y;
+        private double _z;
 
-        public Vector(double x, double y)
+        public Vector(double x, double y, double z)
         {
             _x = x;
             _y = y;
+            _z = z;
         }
 
         public static Vector operator +(Vector first, Vector second)
         {
-            return new Vector(first._x + second._x, first._y + second._y);
+            return new Vector(first._x + second._x, first._y + second._y, first._z + second._z);
         }
 
         public static Vector operator -(Vector first, Vector second)
         {
-            return new Vector(first._x - second._x, first._y - second._y);
+            return new Vector(first._x - second._x, first._y - second._y, first._z - second._z);
         }
 
         public static double operator *(Vector first, Vector second)
         {
-            return first._x * second._x + first._y * second._y;
+            return first._x * second._x + first._y * second._y + first._z * second._z;
         }
         // Это векторное произведение (нет идей, какой здесь ещё знак сделать)
-        public static double operator ^(Vector first, Vector second)
+        public static Vector operator ^(Vector first, Vector second)
         {
-            return first._x * second._y - first._y * second._x;
+            return new Vector(first._y * second._z - first._z * second._y, -(first._z * second._z - first._z * second._x), first._x * second._y - first._y * second._x);
         }
 
-        public double Length => Math.Sqrt((_x * _x) + (_y * _y)); 
+        public double Length => Math.Sqrt((_x * _x) + (_y * _y) + (_z * _y)); 
     }
 }
